aa63b0deddfff9e76452d89566527751a86d5559
HaoNguyen279 <23686691.hao@student.iuh.edu.vn>
2025-06-26 00:19:29 +0700

    test

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ const port = 3000
 const hbs = require('express-handlebars');
 const path = require('path');
 
+// Method static khi gặp path trên url này phải kiểm tra với dạng file tĩnh phải kiểm tra thư mục join đã cung cấp trong phương thức static
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use middleware
+app.use(express.urlencoded({
+    extended: true
+})); // gửi form lên server bằng post hay get đều đc
+app.use(express.json());
+//
+
 //Template engine
 app.engine('hbs', hbs.engine({extname:'.hbs'}));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'))
+app.set('views', path.join(__dirname, 'resources','views'))
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -18,7 +28,7 @@ app.get('/adu', (req, res) => {
 })
 app.get('/', (req,res) =>{
     res.render('home');
-} )
+})
 
 app.get('/news', (req,res) =>{
     console.log(req.query.name);
@@ -30,9 +40,16 @@ app.get('/search', (req,res) =>{
     res.render('search');
 })
 
+
+app.post('/news', (req,res) =>{
+    console.log(req.body)
+    res.send('');
+})
+// Middle dc cài sẵn 1 lib là 'qs' (query string - npm) có nhiệm vụ nhận Form Data từ client gửi lên và parse sang biến body
+
 app.get('/jack', (req,res) => {
-    console.log(res.cookie);
     res.send(`<a href='https://www.google.gg/'>Alo</a>`);
+    
 })
 
 app.listen(port, () => {
